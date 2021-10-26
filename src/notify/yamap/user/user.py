3f473fa25160e46eae793160ef11c9bb4c552c49
goppy <tacumin.goppy.06@gmail.com>
2021-10-27 02:15:16 +0900

    add user_list_id

--- src/notify/yamap/user/user.py
+++ src/notify/yamap/user/user.py
@@ -12,11 +12,16 @@ class Users():
         self.config_ini.read('config.ini', encoding='utf-8')
         self.email = self.config_ini['DEFAULT']['email']
         self.password = self.config_ini['DEFAULT']['password']
+
     
     # トークン取得
     def get_token(self):
         res = self.login()
         return res['user']['token']
+    
+    # ユーザーリストID取得
+    def get_user_list_id(self):
+        return self.config_ini['DEFAULT']['user_list_id']
     
     # ログインする
     def login(self):
@@ -34,3 +39,12 @@ class Users():
     def __get_login_url(self):
         url = 'https://api.yamap.com/session'
         return url
+
+    # email取得
+    def __get_email(self):
+        return self.config_ini['DEFAULT']['email']
+
+    # パスワード取得
+    def __get_password(self):
+        return self.config_ini['DEFAULT']['password']
+
